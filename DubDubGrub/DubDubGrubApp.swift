73ba234729312f60cba20e333f71d940eb0ff60e
John Lollar <jlollar45@gmail.com>
2022-05-18 23:19:58 -0500

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by John Lollar on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
