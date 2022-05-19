23a37386f0a22f75dcf1caded93805f450e107f3
John Lollar <jlollar45@gmail.com>
2022-05-18 23:59:30 -0500

    Initial Setup and added Map to MapView

---
+++ DubDubGrub/Color+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Ext.swift
+//  DubDubGrub
+//
+//  Created by John Lollar on 5/18/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
---
+++ DubDubGrub/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  DubDubGrub
+//
+//  Created by John Lollar on 5/18/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
