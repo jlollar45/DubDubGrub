85e8e06cb31aa06d8f53080fd1231efd5f9c3e8d
John Lollar <jlollar45@gmail.com>
2022-05-29 23:52:03 -0500

    Added basic Profile Form Validation

--- DubDubGrub/Utilities/AlertItem.swift
+++ DubDubGrub/Utilities/AlertItem.swift
@@ -33,4 +33,9 @@ struct AlertContext {
                                             message: Text("Your phone's location services are disabled. To change that, go to your phone's Settings > Privacy > Location Servies"),
                                             dismissButton: .default(Text("Ok")))
     
+    //MARK: - ProfileView Errors
+    static let invalidProfile = AlertItem(title: Text("Invalid Profile"),
+                                                message: Text("All fields are required, as well as a profile photo. Your bio must be < 100 characters.\nPlease try again."),
+                                                dismissButton: .default(Text("Ok")))
+    
 }
